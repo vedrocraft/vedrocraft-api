cb78a4c91bcc95e30b09ae490eef5a9bbd6dd281
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-13 20:33:11 +0300

    BasePlugin, содержит ServiceGetter и DaoFinder

---
+++ bukkit/src/main/java/ru/sema1ary/vedrocraftapi/interfaces/BasePlugin.java
@@ -0,0 +1,8 @@
+package ru.sema1ary.vedrocraftapi.interfaces;
+
+import ru.sema1ary.vedrocraftapi.ormlite.DaoFinder;
+import ru.sema1ary.vedrocraftapi.service.ServiceGetter;
+
+@SuppressWarnings("unused")
+public interface BasePlugin extends DaoFinder, ServiceGetter {
+}

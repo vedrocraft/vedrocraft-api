9eaad92b057e501e3f39ea8d802a1e78e9e716ee
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-13 21:34:36 +0300

    Шаблон Dao для юзеров

---
+++ common/src/main/java/ru/sema1ary/vedrocraftapi/ormlite/UserDao.java
@@ -0,0 +1,23 @@
+package ru.sema1ary.vedrocraftapi.ormlite;
+
+import com.j256.ormlite.dao.Dao;
+import lombok.NonNull;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@SuppressWarnings("unused")
+public interface UserDao <T, ID> extends Dao<T, ID> {
+    T save(@NonNull T user) throws SQLException;
+
+    void saveAll(@NonNull List<T> users) throws SQLException;
+
+    Optional<T> findById(Long id) throws SQLException;
+
+    Optional<T> findByUsername(@NonNull String username) throws SQLException;
+
+    List<T> findAll() throws SQLException;
+
+    T getUser(@NonNull String username);
+}

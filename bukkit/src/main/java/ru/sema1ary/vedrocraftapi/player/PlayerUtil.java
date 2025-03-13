eb3435867f2b8a7944756e47c88cf70368e0163c
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-13 20:42:20 +0300

    Уменьшил код через consumer

--- bukkit/src/main/java/ru/sema1ary/vedrocraftapi/player/PlayerUtil.java
+++ bukkit/src/main/java/ru/sema1ary/vedrocraftapi/player/PlayerUtil.java
@@ -4,7 +4,10 @@ import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+
+import java.util.function.Consumer;
 
 @UtilityClass
 @SuppressWarnings("unused")
@@ -12,20 +15,28 @@ public class PlayerUtil {
     private final MiniMessage miniMessage = MiniMessage.miniMessage();
 
     public void sendMessage(Player player, @NonNull String message) {
-        if(player == null || !player.isOnline()) {
-            return;
-        }
-
-        player.sendMessage(miniMessage.deserialize(message));
+        accept(player, player1 -> player1.sendMessage(miniMessage.deserialize(message)));
     }
 
     public void sendMessage(@NonNull String username, @NonNull String message) {
         Player player = Bukkit.getPlayer(username);
+        accept(player, player1 -> player1.sendMessage(miniMessage.deserialize(message)));
+    }
 
+    public void teleportAsync(Player player, @NonNull Location location) {
+        accept(player, player1 -> player1.teleportAsync(location));
+    }
+
+    public void teleportAsync(String username, @NonNull Location location) {
+        Player player = Bukkit.getPlayer(username);
+        accept(player, player1 -> player1.teleportAsync(location));
+    }
+
+    private void accept(Player player, Consumer<Player> consumer) {
         if(player == null || !player.isOnline()) {
             return;
         }
 
-        player.sendMessage(miniMessage.deserialize(message));
+        consumer.accept(player);
     }
 }

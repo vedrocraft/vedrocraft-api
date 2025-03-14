334d5ccc506088b060d941ff97f92768130eca34
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-14 10:25:17 +0300

    Шаблон UserService

---
+++ common/src/main/java/ru/sema1ary/vedrocraftapi/service/UserService.java
@@ -0,0 +1,19 @@
+package ru.sema1ary.vedrocraftapi.service;
+
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.Optional;
+
+@SuppressWarnings("unused")
+public interface UserService <T> extends Service {
+    T save(@NonNull T user);
+
+    void saveAll(@NonNull List<T> users);
+
+    Optional<T> findById(Long id);
+
+    Optional<T> findByUsername(@NonNull String username);
+
+    List<T> findAll();
+}

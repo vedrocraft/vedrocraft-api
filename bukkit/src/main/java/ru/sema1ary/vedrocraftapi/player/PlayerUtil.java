799c122c8f6a2f515e13d3843baf9b152ce4ddbe
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-24 18:26:41 +0300

    FIIIIIIIIIIIIIXXXX

--- bukkit/src/main/java/ru/sema1ary/vedrocraftapi/player/PlayerUtil.java
+++ bukkit/src/main/java/ru/sema1ary/vedrocraftapi/player/PlayerUtil.java
@@ -1,9 +1,7 @@
 package ru.sema1ary.vedrocraftapi.player;
 
-import lombok.Getter;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
-import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.title.Title;
@@ -19,103 +17,67 @@ import java.util.function.Consumer;
 @UtilityClass
 @SuppressWarnings("unused")
 public class PlayerUtil {
-    @Getter
     private final MiniMessage miniMessage = MiniMessage.miniMessage();
 
     public void sendMessage(Player player, @NonNull Component message) {
         PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.sendMessage(message));
     }
 
-    public void sendMessage(CommandSender player, @NonNull Component message) {
-        PlayerUtil.accept(player, player1 -> player1.sendMessage(message));
+    public void sendMessage(CommandSender sender, @NonNull Component message) {
+        PlayerUtil.accept(sender, player -> player.sendMessage(message));
     }
 
     public void sendMessage(@NonNull String username, @NonNull Component message) {
-        Player player = Bukkit.getPlayer(username);
-        PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.sendMessage(message));
+        PlayerUtil.accept(Bukkit.getPlayer(username), (Consumer<Player>) player1 -> player1.sendMessage(message));
     }
 
     public void sendMessage(Player player, @NonNull String message) {
-        PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.sendMessage(
-                PlayerUtil.getMiniMessage().deserialize(message)
+        accept(player, (Consumer<Player>) player1 -> player1.sendMessage(
+                miniMessage.deserialize(message)
         ));
     }
 
     public void sendMessage(CommandSender player, @NonNull String message) {
-        PlayerUtil.accept(player, player1 -> player1.sendMessage(
-                PlayerUtil.getMiniMessage().deserialize(message)
+        accept(player, player1 -> player1.sendMessage(
+                miniMessage.deserialize(message)
         ));
     }
 
     public void sendMessage(@NonNull String username, @NonNull String message) {
-        Player player = Bukkit.getPlayer(username);
-        PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.sendMessage(
-                PlayerUtil.getMiniMessage().deserialize(message)
+        accept(Bukkit.getPlayer(username), (Consumer<Player>) player1 -> player1.sendMessage(
+                miniMessage.deserialize(message)
         ));
     }
 
-    public void sendMessageWithPAPI(Player player, @NonNull Component message) {
-        sendMessage(player, PlayerUtil.getMiniMessage().deserialize(PlaceholderAPI
-                .setPlaceholders(player, PlayerUtil.getMiniMessage().serialize(message))));
-    }
-
-    public void sendMessageWithPAPI(CommandSender player, @NonNull Component message) {
-        sendMessage(player, PlayerUtil.getMiniMessage().deserialize(PlaceholderAPI
-                .setPlaceholders((Player) player, PlayerUtil.getMiniMessage().serialize(message))));
-    }
-
-    public void sendMessageWithPAPI(@NonNull String username, @NonNull Component message) {
-        Player player = Bukkit.getPlayer(username);
-        sendMessage(player, PlayerUtil.getMiniMessage().deserialize(PlaceholderAPI
-                .setPlaceholders(player, PlayerUtil.getMiniMessage().serialize(message))));
-    }
-
-    public void sendMessageWithPAPI(Player player, @NonNull String message) {
-        sendMessage(player, PlaceholderAPI
-                .setPlaceholders(player, message));
-    }
-
-    public void sendMessageWithPAPI(CommandSender player, @NonNull String message) {
-        sendMessage(player, PlaceholderAPI
-                .setPlaceholders((Player) player, message));
-    }
-
-    public void sendMessageWithPAPI(@NonNull String username, @NonNull String message) {
-        Player player = Bukkit.getPlayer(username);
-        sendMessage(player, PlaceholderAPI
-                .setPlaceholders(player, message));
-    }
-
     public void teleportAsync(Player player, @NonNull Location location) {
-        PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.teleportAsync(location));
+        accept(player, (Consumer<Player>) player1 -> player1.teleportAsync(location));
     }
 
     public void teleportAsync(@NonNull String username, @NonNull Location location) {
         Player player = Bukkit.getPlayer(username);
-        PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.teleportAsync(location));
+        accept(player, (Consumer<Player>) player1 -> player1.teleportAsync(location));
     }
 
     public void teleportAsync(Player player, @NonNull String location) {
-        PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.teleportAsync(LocationSerializer.deserialize(location)));
+        accept(player, (Consumer<Player>) player1 -> player1.teleportAsync(LocationSerializer.deserialize(location)));
     }
 
     public void teleportAsync(@NonNull String username, @NonNull String location) {
         Player player = Bukkit.getPlayer(username);
-        PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.teleportAsync(LocationSerializer.deserialize(location)));
+        accept(player, (Consumer<Player>) player1 -> player1.teleportAsync(LocationSerializer.deserialize(location)));
     }
 
     public void showTitle(Player player, String title, String subtitle) {
-        PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.showTitle(Title.title(
-                        PlayerUtil.getMiniMessage().deserialize(title),
-                        PlayerUtil.getMiniMessage().deserialize(subtitle)
-                )
-        ));
+        accept(player, (Consumer<Player>) player1 -> player1.showTitle(Title.title(
+                miniMessage.deserialize(title),
+                miniMessage.deserialize(subtitle)
+        )));
     }
 
     public void showTitle(Player player, String title, String subtitle, long fadeIn, long stay, long fadeOut) {
-        PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.showTitle(Title.title(
-                PlayerUtil.getMiniMessage().deserialize(title),
-                PlayerUtil.getMiniMessage().deserialize(subtitle),
+        accept(player, (Consumer<Player>) player1 -> player1.showTitle(Title.title(
+                miniMessage.deserialize(title),
+                miniMessage.deserialize(subtitle),
                 Title.Times.times(
                         Duration.ofMillis(fadeIn),
                         Duration.ofMillis(stay), Duration.ofMillis(fadeOut)
@@ -124,9 +86,9 @@ public class PlayerUtil {
     }
 
     public void showInfinityTitle(Player player, String title, String subtitle) {
-        PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.showTitle(Title.title(
-                PlayerUtil.getMiniMessage().deserialize(title),
-                PlayerUtil.getMiniMessage().deserialize(subtitle),
+        accept(player, (Consumer<Player>) player1 -> player1.showTitle(Title.title(
+                miniMessage.deserialize(title),
+                miniMessage.deserialize(subtitle),
                 Title.Times.times(
                         Duration.ofMillis(60),
                         Duration.ofDays(120),
@@ -136,9 +98,9 @@ public class PlayerUtil {
     }
 
     public void showInfinityTitle(Player player, String title, String subtitle, long fadeIn, long fadeOut) {
-        PlayerUtil.accept(player, (Consumer<Player>) player1 -> player1.showTitle(Title.title(
-                PlayerUtil.getMiniMessage().deserialize(title),
-                PlayerUtil.getMiniMessage().deserialize(subtitle),
+        accept(player, (Consumer<Player>) player1 -> player1.showTitle(Title.title(
+                miniMessage.deserialize(title),
+                miniMessage.deserialize(subtitle),
                 Title.Times.times(
                         Duration.ofMillis(fadeIn),
                         Duration.ofDays(120),
@@ -148,7 +110,7 @@ public class PlayerUtil {
     }
 
     public void resetTitle(Player player) {
-        PlayerUtil.accept(player, Player::resetTitle);
+        accept(player, Player::resetTitle);
     }
 
     private void accept(Player player, Consumer<Player> consumer) {
